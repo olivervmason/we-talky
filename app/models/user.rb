b7a869843d0d602add531390eeb0ada0156c51f2
Oliver Mason <omason@protonmail.com>
2020-05-12 16:26:43 +1000

    added user model dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :listings         
+  has_many :listings, dependent: :destroy         
 end
