b1dfa9780634cbc567d16215d3a7704c49444b0d
Oliver Mason <omason@protonmail.com>
2020-05-15 13:21:16 +1000

    Add Language model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,5 @@ class Profile < ApplicationRecord
   validates_uniqueness_of :user_id, message: "you can only have one profile"
   validates :name, :native_language, :target_language, :nationality , presence: true
   has_one_attached :picture
+  has_one :language
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_112725) do
+ActiveRecord::Schema.define(version: 2020_05_15_031819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2020_05_12_112725) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
